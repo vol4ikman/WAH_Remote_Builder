5ae31cfc80420194c1c2288e28f5d99d70a3b70a
Alex Volkov <vol4ikman@gmail.com>
2017-10-09 23:46:12 +0300

    9-Oct update

--- src/components/Builder.vue
+++ src/components/Builder.vue
@@ -27,17 +27,13 @@
 
         <div class="builder-form-wrapper">
 
-            <form class="builder-form" method="post" @submit.prevent="generateCode()">
+            <form class="builder-form" method="post" @submit.prevent>
 
                 <div class="form-row">
                     <h3><span>{{ locale[current_lang].user_settings }}:</span></h3>
                     <label>
                         <span>{{ locale[current_lang].user_id }}:</span>
-                        <input type="text" v-model="json.user.id">
-                    </label>
-                    <label>
-                        <span>{{ locale[current_lang].token }}:</span>
-                        <input type="text" v-model="json.user.token">
+                        <input type="text" v-model="uid">
                     </label>
                 </div>
 
@@ -77,6 +73,11 @@
                             <option value="ru">{{ locale[current_lang].russian_label }}</option>
                             <option value="he">{{ locale[current_lang].hebrew_label }}</option>
                         </select>
+                    </label>
+
+                    <label class="inline">
+                        <input type="checkbox" v-model="json.setup.jquery">
+                        <span v-html="locale[current_lang].jquery"></span>
                     </label>
 
                     <label class="inline">
@@ -120,8 +121,6 @@
                     </label>
                 </div>
 
-                <button type="submit" class="submit">{{ locale[current_lang].generate_code }}</button>
-
             </form>
 
             <div class="builder-live" v-if="code">
@@ -137,9 +136,9 @@
                         'background' : defaultLivePreviewBG,
                         'color' : defaultLivePreviewColor
                     }">
-                    <div class="builder-lorem">
-                        <p>{{ locale[current_lang].lorem_ipsum }}</p>
-                    </div>
+
+                    <div class="builder-lorem" v-html="locale[current_lang].lorem_ipsum"></div>
+
                     <div id="wp_access_helper_container" class="accessability_container active"
                         :class="{
                             'dark_theme' : json.theme.name == 'dark',
@@ -258,9 +257,6 @@
                 <h3><span>{{ locale[current_lang].code_title }}:</span></h3>
                 <textarea v-model="code" @click="selectCode()" ref="codeEditor"></textarea>
                 <div class="notice-wrapper" v-if="code">
-                    <div class="notice" v-if="!json.user.id">
-                        {{ locale[current_lang].error_id_label }}
-                    </div>
                     <div class="copied" v-if="copied">
                         {{ locale[current_lang].notice_copied }}
                     </div>
@@ -272,7 +268,17 @@
                     <span>{{ locale[current_lang].step3_title }}</span>
                 </div>
 
-                <div class="step-description step3-description" v-html="locale[current_lang].step3_description"></div>
+                <div class="step-description step3-description">
+                    <p v-html="locale[current_lang].step3_description"></p>
+                    <div class="code-editor" contenteditable="true">
+                        <p v-html="step3_scripts_jquery" v-if="!json.setup.jquery"></p>
+                        <p v-html="locale[current_lang].step3_script_config"></p>
+                        <p>&lt;script src="https://accessibility-helper.co.il/remote/script/wahpro?uid={{uid}}" charset="utf-8"&gt;&lt;/script&gt;</p>
+                    </div>
+                    <div class="notice" v-if="!uid">
+                        {{ locale[current_lang].error_id_label }}
+                    </div>
+                </div>
             </div>
 
         </div>
@@ -287,6 +293,7 @@ export default {
     name: 'hello',
     data () {
         return {
+            uid                     : '',
             code                    : '',
             copied                  : false,
             current_lang            : 'en',
@@ -294,24 +301,25 @@ export default {
             is_rtl                  : false,
             defaultLivePreviewBG    : 'black',
             defaultLivePreviewColor : 'white',
+            step3_scripts_jquery    : '&lt;script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/core.js"&gt;',
             locale : {
                 en : {
-                    lorem_ipsum                  : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    lorem_ipsum                  : '<h1>Lorem title</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
                     step1_title                  : 'Step 1 - Generate script code',
                     step2_title                  : 'Step 2 - Copy code',
                     step3_title                  : 'Step 3 - Insert code template into your page',
-                    step3_description            : '<p><strong>Paste the code just right before closing body tag &lt;/body&gt;.</p><p>jQuery library is required! So if you do not have it, please include it before calling WAH PRO script.</strong></p><p>The example of result code:</p><div class="code-editor"><p>&lt;script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/core.js"&gt;</p><p>&lt;script&gt;&lt;!-- Paste your code from step 2 HERE --&gt;&lt;/script&gt;</p><p>&lt;script src="https://accessibility-helper.co.il/remote/script/wahpro.src.js" charset="utf-8"&gt;&lt;/script&gt;</p></div><p><strong>Please, double check your User ID, if User ID is not valid - WAH PRO remote script will NOT work.</strong></p>',
+                    step3_script_config          : '&lt;script&gt;&lt;!-- Paste your code from step 2 HERE --&gt;&lt;/script&gt;',
+                    step3_description            : '<p><strong>Paste the code just right before closing body tag &lt;/body&gt;.</p><p>jQuery library is required! So if you do not have it, please include it before calling WAH PRO script.</strong></p><p><strong>Please, double check your User ID, if User ID is not valid - WAH PRO remote script will NOT work.</strong></p><p>The example of result code:</p>',
                     title                        : 'Welcome to WAH PRO Remote script Builder',
                     subtitle                     : 'WP Accessibility Helper by WAH TEAM',
                     user_settings                : 'User settings',
-                    user_id                      : 'User ID',
-                    token                        : 'Token',
+                    user_id                      : 'User ID (required)',
                     theme_settings               : 'Theme Settings',
                     theme_name                   : 'Theme color',
                     layout                       : 'Layout',
                     sidebar_position             : 'Sidebar position',
                     setup_settings               : 'Setup Settings',
-                    language                     : 'Language',
+                    language                     : 'Plugin Language',
                     enable_debug                 : 'Enable debug?',
                     enable_statistics            : 'Enable statistics?',
                     disable_remove_animations    : 'Disable "Remove animations" button?',
@@ -321,7 +329,6 @@ export default {
                     disable_invert_colors        : 'Disable "Invert colors" button?',
                     disable_readable_fonts       : 'Disable "Readable fonts" button?',
                     code_title                   : 'Code',
-                    generate_code                : 'Generate code & Preview',
                     english_label                : 'English',
                     russian_label                : 'Russian',
                     hebrew_label                 : 'Hebrew',
@@ -335,6 +342,7 @@ export default {
                     error_id_label               : 'Important notice: User ID is required.',
                     notice_copied                : 'Code has been copied!',
                     live_preview_title           : 'Live preview',
+                    jquery                       : 'I have <a href="https://jquery.com" target="_blank"><strong>jQuery</strong></a> installed on my website',
                     live : {
                         close               : 'Close',
                         font_resize         : 'Font Resize',
@@ -352,16 +360,16 @@ export default {
                     }
                 },
                 ru : {
-                    lorem_ipsum                  : 'Лорем ипсум долор сит амет, ут сит суас граецис трацтатос, ид дицта сонет модератиус сеа, ерос импедит хас ид. Реяуе тантас ментитум но про, еум ид воцент плацерат цонсеяуат. Нам ессе волутпат петентиум ут. Юсто нихил цонституам ан сед. Не цонвенире интерессет вел, дисцере алияуид феугаит при ид.',
+                    lorem_ipsum                  : '<h1>Лорем ипсум заголовок</h1><p>Лорем ипсум долор сит амет, ут сит суас граецис трацтатос, ид дицта сонет модератиус сеа, ерос импедит хас ид. Реяуе тантас ментитум но про, еум ид воцент плацерат цонсеяуат. Нам ессе волутпат петентиум ут. Юсто нихил цонституам ан сед. Не цонвенире интерессет вел, дисцере алияуид феугаит при ид.</p>',
                     step1_title                  : 'Шаг 1 - Заполнить форму и получить код.',
                     step2_title                  : 'Шаг 2 - Скопировать код',
                     step3_title                  : 'Шаг 3 - Вставить код на вашу страничку',
-                    step3_description            : '<p>Вставить данный код перед закрытием тэга body</p><p>Библиотека jQuery должна быть добавлена на вашу страничку! В случае отсутсвия данной библиотеки скрипт работать не будет.</strong></p><p>Ваш скрипт должен выглядеть следующим образом:</p><div class="code-editor"><p>&lt;script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/core.js"&gt;</p><p>&lt;script&gt;&lt;!-- Вставьте скрипт полученный в шаге 2 сюда --&gt;&lt;/script&gt;</p><p>&lt;script src="https://accessibility-helper.co.il/remote/script/wahpro.src.js" charset="utf-8"&gt;&lt;/script&gt;</p></div><p><strong>Пожалуйста, перепроверьте дважды ваш User ID, в противной случае скрипт работать не будет.</strong></p>',
+                    step3_script_config          : '&lt;script&gt;&lt;!-- Вставьте код полученный в шаге 2 сюда --&gt;&lt;/script&gt;',
+                    step3_description            : '<p>Вставить данный код перед закрытием тэга body</p><p>Библиотека jQuery должна быть добавлена на вашу страничку! В случае отсутсвия данной библиотеки скрипт работать не будет.</strong></p><p>Ваш скрипт должен выглядеть следующим образом:</p><p><strong>Пожалуйста, перепроверьте дважды ваш User ID, в противной случае скрипт работать не будет.</strong></p>',
                     title                        : 'Добро пожаловать в WAH PRO Remote script Builder',
                     subtitle                     : 'WP Accessibility Helper by WAH TEAM',
                     user_settings                : 'Настройки пользователя',
-                    user_id                      : 'ID пользователя',
-                    token                        : 'Токен (секретный ключ)',
+                    user_id                      : 'ID пользователя (обязательно)',
                     theme_settings               : 'Настройки шаблона',
                     theme_name                   : 'Цвет шаблона',
                     layout                       : 'Структура шаблона',
@@ -377,7 +385,6 @@ export default {
                     disable_invert_colors        : 'Отключить кнопку "Искажение цвета"?',
                     disable_readable_fonts       : 'Отключить кнопку "Читаемый шрифт"?',
                     code_title                   : 'Код',
-                    generate_code                : 'Генерировать код',
                     english_label                : 'Английский (США и Великобритания)',
                     russian_label                : 'Русский (Россия)',
                     hebrew_label                 : 'Иврит (Израиль)',
@@ -391,6 +398,7 @@ export default {
                     error_id_label               : 'Внимание! "User ID" это обязательный параметр. Без него ваш скрипт работать НЕ будет.',
                     notice_copied                : 'Код скопирован в буфер обмена!',
                     live_preview_title           : 'Предпросмотр',
+                    jquery                       : '<a href="https://jquery.com" target="_blank"><strong>jQuery</strong></a> уже установлен на моем сайте',
                     live : {
                         close               : 'Закрыть',
                         font_resize         : 'Размер шрифта',
@@ -408,16 +416,16 @@ export default {
                     }
                 },
                 he : {
-                    lorem_ipsum                  : 'לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון בלרק - וענוף לפרומי בלוף קינץ תתיח לרעח. לת צשחמי צש בליא, מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק.',
+                    lorem_ipsum                  : '<h1>לורם איפסום כותרת</h1><p>לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון בלרק - וענוף לפרומי בלוף קינץ תתיח לרעח. לת צשחמי צש בליא, מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק.</p>',
                     step1_title                  : 'שלב 1 - למלא טופס ולקבל קוד התמאה.',
                     step2_title                  : 'שלב 2 - להעתיק קוד',
                     step3_title                  : 'שלב 3 - להדביר קוד לעמוד',
-                    step3_description            : '<p>תדביקו קוד שקבלתם ישר אחרי סגירה של תגית body.</p><p><strong>חייבים להוסיף ספריית jQuery לעמוד שלכם, אחרת הסקריפט לא יעבוד!</strong></p><p>קוד שלכם אמור להראות בצורה בהא:</p><div class="code-editor"><p>&lt;script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/core.js"&gt;</p><p>&lt;script&gt;&lt;!-- הדביקו קוד שקיבלתם בשלב 2 לכאן --&gt;&lt;/script&gt;</p><p>&lt;script src="https://accessibility-helper.co.il/remote/script/wahpro.src.js" charset="utf-8"&gt;&lt;/script&gt;</p></div><p><strong>בבקשה, תבדקו פעמים את User ID שלכם, אחרת סקריפט לא יעבוד.</strong></p>',
+                    step3_script_config          : '&lt;script&gt;&lt;!-- הדביקו קוד שקיבלתם בשלב 2 לכא --&gt;&lt;/script&gt;',
+                    step3_description            : '<p>תדביקו קוד שקבלתם ישר אחרי סגירה של תגית body.</p><p><strong>חייבים להוסיף ספריית jQuery לעמוד שלכם, אחרת הסקריפט לא יעבוד!</strong></p><p>קוד שלכם אמור להראות בצורה בהא:</p><p><strong>בבקשה, תבדקו פעמים את User ID שלכם, אחרת סקריפט לא יעבוד.</strong></p>',
                     title                        : 'ברוכים הבאים ל WAH PRO Remote script Builder',
                     subtitle                     : 'WP Accessibility Helper by WAH TEAM',
                     user_settings                : 'הגדרות משתמש',
-                    user_id                      : 'מזהה של משתמש',
-                    token                        : 'מפתח',
+                    user_id                      : 'מזהה של משתמש (שדה חובה)',
                     theme_settings               : 'הגדרות תבנית',
                     theme_name                   : 'צבע של תבנית',
                     layout                       : 'פריסת תבנית',
@@ -433,7 +441,6 @@ export default {
                     disable_invert_colors        : 'הסר כפתור "היפוך צבעים"?',
                     disable_readable_fonts       : 'הסר כפתור "פונט קריא"?',
                     code_title                   : 'קוד',
-                    generate_code                : 'הצג קוד ותצוגה מקדימה',
                     english_label                : 'אנגלית',
                     russian_label                : 'רוסית',
                     hebrew_label                 : 'עברית',
@@ -447,6 +454,7 @@ export default {
                     error_id_label               : 'שימו לב! מזהה של משתמש הוא שדה חובה. ללא מזהה תקין קוד לא יעבוד.',
                     notice_copied                : 'קוד הועתק בהצלחה!',
                     live_preview_title           : 'תצוגה מקדימה',
+                    jquery                       : '<a href="https://jquery.com" target="_blank"><strong>jQuery</strong></a> כבר מותקן באתר',
                     live : {
                         close               : 'סגור',
                         font_resize         : 'גודל פונט',
@@ -465,20 +473,16 @@ export default {
                 }
             },
             json: {
-                user : {
-                    id: '',                // user ID
-                    token : ''           // user secret token
-                },
                 theme: {
                     name     : 'dark',           // dark || light
                     position : 'right',          // right || left
                     type     : 1,                // 1 = standart, 2 = wide, 3 = magic
-                    //icon     : 'style/banner.jpg'               // url to image 48x48px
                 },
                 setup: {
-                    debug    : true,               // true or false
-                    stats    : true,
-                    lang     : 'en',               // en, ru, he [default en]
+                    debug    : true,              // true or false
+                    stats    : true,              // true or false
+                    lang     : 'en',              // en, ru, he [default en]
+                    jquery   : false,             // true or false
                     hide : {
                         remove_animations   : false,
                         highlight_titles    : false,
@@ -488,10 +492,6 @@ export default {
                         readable_fonts      : false
                     }
                 }
-            },
-            user: {
-                id    : '',
-                token : ''
             }
         }
     },
@@ -523,6 +523,17 @@ export default {
             this.defaultLivePreviewBG = bg;
             this.defaultLivePreviewColor = color;
         }
+    },
+    watch: {
+        json: {
+            handler: function (val, oldVal) {
+                this.generateCode();
+            },
+            deep: true
+        }
+    },
+    mounted() {
+        this.generateCode();
     }
 }
 </script>
